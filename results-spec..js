c10558f470f1c22402a1bab2f9b84958f904bc1d
a-inglese <ainglese@despegar.com>
2018-08-21 07:25:36 -0300

    skeleton

--- results-spec..js
+++ results-spec..js
@@ -6,18 +6,7 @@ describe('Test de Resultados', function() {
         this.resultsPage = new results();
     });
 
-    it('should have at least 1 cluster visible', function() {
-        this.resultsPage.getClusters().then(function(clusters) {
-            expect(clusters[0].isDisplayed()).toBe(true);
-        });
-    });
-
-    it('should have at least 10 clusters and less than 100', function() {
-      expect(this.resultsPage.getClustersCount()).toBeGreaterThan(10);
-      expect(this.resultsPage.getClustersCount()).toBeLessThan(100);
-    });
-
-    it('should have the filter component', function() {
-      expect(this.resultsPage.getFilters().isDisplayed()).toBe(true);
+    it('should have at least 1 cluster', function() {
+      expect(this.resultsPage.getClustersCount()).toBeGreaterThan(0);
     });
 });
